b37731f42a719048be06d852a2863b1d1566f90e
samuelRobRodrigues <samuelRoberto_dev@outlook.com>
2023-12-11 17:02:44 -0300

    objetcs

--- instanciaseclasses.js
+++ instanciaseclasses.js
@@ -1,16 +1,28 @@
-class pessoa {
+class Pessoa {
     nome;
     idade;
 
     constructor(nome, idade) {
         this.nome = nome;
         this.idade = idade;
+        this.anoDoNascimento = 2023 - idade;
     }
         descrever() {
             console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`)
         }
 }
-const samuel = new pessoa('Samuel', 21);
-const mario = new pessoa('Mario', 21)
-samuel.descrever()
-mario.descrever()
+
+function compararPessoas(p1, p2) {
+    if (p1.idade > p2.idade) {
+        console.log(`${p1.nome} é mais velho que ${p2.nome}`)
+    } else if (p2.idade > p1.idade) {
+        console.log(`${p2.nome} é mais velho que ${p1.nome}`)
+    } else {
+        console.log(`${p1.nome} e ${p2.nome} tem a mesma idade`)
+    }
+}
+
+const samuel = new Pessoa('Samuel', 21);
+const mario = new Pessoa('Mario', 41);
+
+compararPessoas(samuel, mario);
