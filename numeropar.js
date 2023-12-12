61c86247812a78d96ed50b52711b64e4b35adb8d
samuelRobRodrigues <samucada160@gmail.com>
2023-12-12 01:12:19 -0300

    update

--- numeropar.js
+++ numeropar.js
@@ -1,8 +1,1 @@
-const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-    for (let i = 0; i < numeros.length; i++) {
-        const numero = numeros[i];
-        if (numero % 2 === 0) {
-            console.log(`${numero} Número par!`)
-        }
-    }
+addEventListener
